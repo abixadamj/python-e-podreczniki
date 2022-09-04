b5a373b12b7bd9a34d732f53063ba17d53a6779e
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-09-04 12:58:38 +0200

    N13 - changes in excersises

--- Podstawowa_klasy-7-8/N13_If-else/if_cwiczenia.py
+++ Podstawowa_klasy-7-8/N13_If-else/if_cwiczenia.py
@@ -1,12 +1,31 @@
+## badanie podzielności liczby
+
+
 wprowadzona_liczba = int(input("Podaj dowolną liczbę naturalną:"))
 reszta = wprowadzona_liczba % 2
 if reszta == 0:
     print("Wprowadzona liczba jest parzysta.")
 else:
     print("Wprowadzona liczba jest nieparzysta.")
-    print(f"Reszta z dzielenia przez 2 wynosi: {reszta}")
+    print("Reszta z dzielenia przez 2 wynosi:", reszta)
+
+
+## Napisz program informujący czy liczba podana przez użytkownika jest
+## większa, mniejsza czy równa zero. Pamiętaj o odpowiednich funkcjach
+## wczytywania danych od użytkownika, o zmianie typu danych.
+## Przemyśl, jakie instrukcje warunkowe i w jakiej kolejności możesz zastosować.
+
+wprowadzona_liczba = int(input("Podaj dowolną liczbę całkowitą:"))
+if wprowadzona_liczba > 0:
+    print("Wprowadzona liczba jest dodatnia.")
+if wprowadzona_liczba < 0:
+    print("Wprowadzona liczba jest ujemna.")
+if wprowadzona_liczba == 0:
+    print("Wprowadzona liczba to zero.")
+
 
 ## silnik samochodu
+
 
 print("Rozpoczynamy sekwencję uruchamiania silnika.")
 
@@ -24,7 +43,23 @@ else:
     print("Bez paliwa nie ruszymy ;-(")
 
 
-# komunikator internetowy
+## komunikator internetowy, w serwisie ZPE.gov.pl funkcje działają, tu zapiszę
+## ich definicje, aby kod był możliwy do uruchomienia w IDLE
+## dostępne dane: login: "linux", hasło: "python"
+
+
+def internet_dostepny():
+    return True
+
+def kontakt_z_serwerem():
+    return True
+
+def prawidlowy_login(login):
+    return True if login == "linux" else False
+
+def prawidlowe_haslo(login, haslo):
+    return True if login == "linux" and haslo == "python" else False
+
 
 print("Rozpoczynamy uruchomienie komunikatora.")
 
@@ -33,11 +68,17 @@ if internet_dostepny():
     if kontakt_z_serwerem():
         print("Serwer dostępny.")
         login = input("Teraz podaj swój LOGIN UŻYTKOWNIKA: ")
+        # w testerce ZPE musi być przypisanie wartości, bo input() nie działa
+        # login = "linux"
         if prawidlowy_login(login):
             haslo = input("A teraz podaj HASŁO: ")
+            # w testerce ZPE musi być przypisanie wartości, bo input() nie działa
+            # haslo = "python"
             if prawidlowe_haslo(login, haslo):
-                print(f"Witaj - {login} - w naszej sieci społecznościowej.")
+                print("Witaj -", login,"- w naszej sieci społecznościowej.")
                 print("Polecam się - https://Matrix.org")
+            else:
+                print("Niestety, hasło jest błędne.")
         else:
             print("Niestety, nie znam takiego użytkownika")
 else:
